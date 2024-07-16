94ef4d1eab8ca05b3a35a7f8cdaddf42b976d48b
ankita-patil-labtest <ankita.patil@nagarro.com>
2024-07-16 15:35:07 +0530

    Update DiscountCalculatorService.java

--- src/java/main/com/asssesment/service/DiscountCalculatorService.java
+++ src/java/main/com/asssesment/service/DiscountCalculatorService.java
@@ -1,1 +1,33 @@
+package com.assesment.service;
 
+@Service
+public class DiscountCalculatorService {
+public double calculateNetPayableAmount(User user) {
+        double totalDiscount = 0.0;
+
+		if (!user.isGrocery()) {
+            if (user.isEmployee()) {
+                totalDiscount = applyMaxPercentageDiscount(user.getBillAmount(), 30);
+            } else if (user.isAffiliate()) {
+                totalDiscount = applyMaxPercentageDiscount(user.getBillAmount(), 10);
+            } else if (user.isLoyalCustomer()) {
+                totalDiscount = applyMaxPercentageDiscount(user.getBillAmount(), 5);
+            }
+        }
+
+        totalDiscount = totalDiscount + calculateBillDiscount(user.getBillAmount());
+
+        //for net payable amount
+        double netPayableAmount = user.getBillAmount() - totalDiscount;
+        return netPayableAmount;
+    }
+
+    private double applyMaxPercentageDiscount(double billAmount, int percentage) {
+        double maxDiscountableAmount = (percentage / 100.0) * billAmount;
+        return maxDiscountableAmount;
+    }
+
+    private double calculateBillDiscount(double billAmount) {
+        return billAmount / 100 * 4.5;
+    }
+}
