2215de6e73ef493eb8256b9408cd44de2a9fe3d0
ankita-patil-labtest <ankita.patil@nagarro.com>
2024-07-16 17:16:58 +0530

    Delete src directory

--- src/java/main/com/asssesment/TestApplication.java
+++
@@ -1,9 +0,0 @@
-package com.assesment;
-import org.springframework.boot.SpringApplication;
-
-@SpringBootApplication
-public class TestApplication{
-    public static void Main(String[] args){
-      SpringApplication.run(TestApplication.class, args);
-    }
-}
