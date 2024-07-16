2e85db3520046f3c8db2f320bf50f941dffc45ee
ankita-patil-labtest <ankita.patil@nagarro.com>
2024-07-16 15:48:38 +0530

    Update TestApplicationTest.java

--- src/java/test/com/asssesment/TestApplicationTest.java
+++ src/java/test/com/asssesment/TestApplicationTest.java
@@ -1,3 +1,10 @@
+package com.assesment;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class TestApplicationTest{
